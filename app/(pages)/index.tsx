85040b0ade022565c308f9eaf7c6e8c6a1018a93
knextkoder <knextkoderklan@gmail.com>
2025-09-13 10:09:10 +0100

    Remove unused iOS project files and update package dependencies
    - Deleted Xcode scheme and workspace files as they are no longer needed.
    - Removed AppDelegate.swift and Info.plist to streamline the project structure.
    - Cleared out image assets related to the app icon and splash screen.
    - Removed privacy info and entitlements files that are not in use.
    - Updated package.json to modify scripts and dependencies, including version adjustments for various packages.

--- app/(pages)/index.tsx
+++ app/(pages)/index.tsx
@@ -4,9 +4,9 @@ export default function HomePage() {
   return (
     <View className="flex-1 items-center justify-center bg-white dark:bg-black">
       <View className="mt-2 mx-5 items-center">
-          <Text className="text-center text-blue-500 dark:text-red-500">
-            Marvel was here baby
-          </Text>
+        <Text className="text-center text-blue-500 dark:text-red-500">
+          Marvel was here baby
+        </Text>
       </View>
     </View>
   );
